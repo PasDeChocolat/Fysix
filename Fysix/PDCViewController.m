e49cad9749dfe2d3ee0fa0e79bf0af4fd4a19293
Kyle Oba <koba@mudphone.com>
2014-03-01 18:12:00 -1000

    Use storyboard, instead of code to set up outlets and actions.

--- Fysix/PDCViewController.m
+++ Fysix/PDCViewController.m
@@ -11,34 +11,17 @@
 @interface PDCViewController ()
 @property (strong, nonatomic) UIDynamicAnimator *animator;
 @property (strong, nonatomic) NSArray *dynamicItems;
-@property (strong, nonatomic) UIView *anchorView;
-@property (strong, nonatomic) UIView *tailView;
+@property (strong, nonatomic) IBOutlet UIView *anchorView;
+@property (strong, nonatomic) IBOutlet UIView *tailView;
 @property (strong, nonatomic) UIAttachmentBehavior *attachmentBehavior;
 @end
 
 @implementation PDCViewController
 
-- (NSArray *)dynamicItems
-{
-    if (!_dynamicItems) {
-        self.anchorView = [[UIView alloc] initWithFrame:CGRectMake(200, 200, 50, 50)];
-        self.anchorView.backgroundColor = [UIColor magentaColor];
-        self.tailView = [[UIView alloc] initWithFrame:CGRectMake(300, 300, 50, 50)];
-        self.tailView.backgroundColor = [UIColor colorWithWhite:0.5 alpha:1.0];
-        _dynamicItems = @[self.anchorView, self.tailView];
-    }
-    return _dynamicItems;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    // init views
-    for (UIView *v in self.dynamicItems) {
-        [self.view addSubview:v];
-    }
     
     // Add animator:
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
@@ -48,13 +31,11 @@
                                initWithItem:self.tailView
                                attachedToAnchor:self.anchorView.center];
     [self.animator addBehavior:self.attachmentBehavior];
-    
-    [self.anchorView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleAttachmentGesture:)]];
 }
 
-- (void)handleAttachmentGesture:(UIPanGestureRecognizer *)gesture
+- (IBAction)handleAttachmentGesture:(UIPanGestureRecognizer *)sender
 {
-    CGPoint gesturePoint = [gesture locationInView:self.view];
+    CGPoint gesturePoint = [sender locationInView:self.view];
     self.anchorView.center = gesturePoint;
     [self.attachmentBehavior setAnchorPoint:gesturePoint];
 }
